f7fe468f2c829a68b934ed7b229bcc764c1585db
Davenji-official <narcissedave2000@gmail.com>
2026-08-03 07:14:19 -0500

    Add Supabase configuration class and constants

---
+++ lib/core/supabase/supabase_config.dart
@@ -0,0 +1,11 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseConfig {
+  SupabaseConfig._();
+
+  static const String url =
+      'https://fzakmezaovalxhwldoeb.supabase.co';
+
+  static const String publishableKey =
+      'REMPLACE_PAR_TA_CLE_PUBLISHABLE';
+}
