ccf7b97cec4a4ec2e3f0e6b434aa712a83f8971e
Davenji-official <narcissedave2000@gmail.com>
2026-08-03 07:55:10 -0500

    fix: import Supabase auth exception

--- lib/features/auth/presentation/register_screen.dart
+++ lib/features/auth/presentation/register_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../data/auth_service.dart';
 
