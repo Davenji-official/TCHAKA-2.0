4ed5875c7cee393db17f8819d6b27087a638b23f
Davenji-official <narcissedave2000@gmail.com>
2026-08-03 12:48:03 -0500

    Add profile routes to the GoRouter

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import '../features/auth/presentation/auth_gate.dart';
 import '../features/auth/presentation/login_screen.dart';
 import '../features/auth/presentation/register_screen.dart';
+import '../features/profile/presentation/profile_screen.dart';
 
 final GoRouter tchakaRouter = GoRouter(
   initialLocation: '/',
@@ -23,6 +24,22 @@ final GoRouter tchakaRouter = GoRouter(
       path: '/register',
       builder: (context, state) {
         return const RegisterScreen();
+      },
+    ),
+    GoRoute(
+      path: '/profile',
+      builder: (context, state) {
+        return const PublicProfileScreen();
+      },
+    ),
+    GoRoute(
+      path: '/profile/:userId',
+      builder: (context, state) {
+        final userId = state.pathParameters['userId'];
+
+        return PublicProfileScreen(
+          userId: userId,
+        );
       },
     ),
   ],
