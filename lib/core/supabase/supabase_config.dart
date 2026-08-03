c76ca58ff963876cd50787cdc338f6b18a9ba4b5
Davenji-official <narcissedave2000@gmail.com>
2026-08-03 07:23:54 -0500

    Refactor SupabaseConfig class formatting

--- lib/core/supabase/supabase_config.dart
+++ lib/core/supabase/supabase_config.dart
@@ -3,9 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class SupabaseConfig {
   SupabaseConfig._();
 
-  static const String url =
-      'https://fzakmezaovalxhwldoeb.supabase.co';
+  static const String url = 'https://fzakmezaovalxhwldoeb.supabase.co';
 
-  static const String publishableKey =
-      'REMPLACE_PAR_TA_CLE_PUBLISHABLE';
+  static const String publishableKey = 'REMPLACE_PAR_TA_CLE_PUBLISHABLE';
 }
