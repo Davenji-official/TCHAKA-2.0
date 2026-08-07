c17c06bad6cd46545b8c17b2aa4f2eb3a799075c
Davenji-official <narcissedave2000@gmail.com>
2026-08-07 09:22:03 -0500

    test: fix widget smoke test for TchakaApp

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:tchaka/main.dart';
+
+void main() {
+  testWidgets('TCHAKA app boots', (WidgetTester tester) async {
+    await tester.pumpWidget(const TchakaApp());
+    await tester.pump();
+
+    expect(find.byType(TchakaApp), findsOneWidget);
+  });
+}
