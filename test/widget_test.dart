859f051159996213dc5b66df6bb73c2eef7f250e
Davenji-official <narcissedave2000@gmail.com>
2026-08-07 13:20:22 -0500

    Create widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tchaka/main.dart';
+
+void main() {
+  testWidgets(
+    'TCHAKA démarre correctement',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(const TchakaApp());
+
+      await tester.pumpAndSettle();
+
+      expect(find.byType(TchakaApp), findsOneWidget);
+    },
+  );
+}
