70c5b5f3ab0d4e1f348e4d0e29fcc3b7ec96d9a0
Davenji-official <narcissedave2000@gmail.com>
2026-08-07 19:46:11 -0500

    Remove unused import from widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:tchaka/app/app.dart';
